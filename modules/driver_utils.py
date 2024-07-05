6122056c860150c3d338b10982e4fe8827f74649
godfreykaris <godfreykarisw@gmail.com>
2024-07-05 13:09:57 +0300

    Handling quarantined subreddits

--- modules/driver_utils.py
+++ modules/driver_utils.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from modules.config import Config
 import modules.shared as shared
 import time
@@ -58,6 +60,27 @@ class DriverUtils:
             except Exception as e:
                 # print(f"Error quitting driver for category {category}: {e}") -------> For Debugging
                 pass
+    
+    @staticmethod
+    def accept_quarantined(driver):
+        try:
+            # Wait for the guard-community-modal to be present
+            community_modal = WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.TAG_NAME, 'guard-community-modal'))
+            )
+
+            # Check if the modal exists
+            if community_modal:
+
+                # Find the "Yes, Continue" button by its text and click it
+                yes_continue_button = WebDriverWait(driver, 5).until(
+                    EC.element_to_be_clickable((By.XPATH, "//div[@slot='secondaryButton']//span[text()='Yes, Continue']/.."))
+                )
+                yes_continue_button.click()
+
+        except Exception as e:
+            # print(f"An error occurred when checking quarantined: {e}") -------> For Debugging
+            pass
 
     @staticmethod
     def access_subreddit(driver):
@@ -69,6 +92,9 @@ class DriverUtils:
         """
         driver.get(shared.reddit_url)  # Load the URL specified in shared.reddit_url
         time.sleep(random.uniform(4.0, 5.3))  # Wait for approximately 5 seconds to allow the page to load
+
+        # Accept access to Quarantined Subreddits
+        DriverUtils.accept_quarantined(driver)
 
     @staticmethod
     def scroll_to_bottom(driver):
